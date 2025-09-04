8192cc206ed01e19c203c6559bee15e812c1d376
Mini <mini@mivra.net>
2025-09-04 19:22:51 +0100

    refactor: Use ComputerCraft http API for public IP retrieval

--- get_public_ip.lua
+++ get_public_ip.lua
@@ -1,13 +1,15 @@
--- Prints the public IP address by calling an external service.
--- Requires 'powershell' to be available in the system's PATH.
+-- Prints the public IP address using the ComputerCraft http API.
 
-local command = 'powershell -NoProfile -Command "Invoke-RestMethod http://icanhazip.com"'
-local handle = io.popen(command)
-if handle then
-  local ip = handle:read('*a')
-  handle:close()
-  -- Trim leading/trailing whitespace (including newlines) from the output
+-- Make a GET request to icanhazip.com
+local response = http.get("http://icanhazip.com")
+
+if response then
+  -- Read the entire response body
+  local ip = response.readAll()
+  response.close()
+  
+  -- The response includes a trailing newline, so we trim whitespace before printing.
   print(ip:match('^%s*(.-)%s*$'))
 else
-  print("Error: Could not execute command. Is PowerShell available in your PATH?")
+  print("Error: HTTP request failed.")
 end

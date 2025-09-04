57de70e913865567f2d656f526d8f74e891b2307
Mini <mini@mivra.net>
2025-09-04 19:19:51 +0100

    fix: Use luasocket for public IP retrieval

--- get_public_ip.lua
+++ get_public_ip.lua
@@ -1,10 +1,12 @@
 -- Prints the public IP address by calling an external service.
--- Requires 'curl' to be installed and in the system's PATH.
+-- Requires the 'luasocket' library to be installed.
 
-local handle = io.popen('curl -s icanhazip.com')
-if handle then
-  local ip = handle:read('*a')
-  handle:close()
+local http = require("socket.http")
+local body, status = http.request("http://icanhazip.com")
+
+if body then
   -- Trim leading/trailing whitespace (including newlines) from the output
-  print(ip:match('^%s*(.-)%s*$'))
+  print(body:match('^%s*(.-)%s*$'))
+else
+  print("Error: Could not retrieve public IP address. Details: " .. tostring(status))
 end
